6f0b8716aeabead7d7287766192798ed606d4211
Dmitry Sharkov <dsharkov@gmail.com>
2014-05-15 01:47:22 -0400

    created car domain class and some fields on it

---
+++ grails-app/domain/com/careworkstech/carworks/Car.groovy
@@ -0,0 +1,12 @@
+package com.careworkstech.carworks
+
+class Car {
+    String make
+    String model
+    String trim
+    Integer year
+    //Condition condition
+
+    static constraints = {
+    }
+}
