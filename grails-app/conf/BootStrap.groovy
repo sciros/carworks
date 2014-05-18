9fbbb8b76c8b31608786d0f39d6f63a0e456a18d
Dmitry Sharkov <dsharkov@gmail.com>
2014-05-17 21:20:44 -0400

    Added security to CarController and role and user creation to BootStrap

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,31 @@
+import com.careworkstech.carworks.Role
+import com.careworkstech.carworks.User
+import com.careworkstech.carworks.UserRole
+
 class BootStrap {
 
     def init = { servletContext ->
+        def userRole = Role.findByAuthority('ROLE_USER') ?: new Role(authority: 'ROLE_USER').save(failOnError: true)
+        def adminRole = Role.findByAuthority('ROLE_ADMIN') ?: new Role(authority: 'ROLE_ADMIN').save(failOnError: true)
+
+        def user1 = User.findByUsername('user1') ?: new User(
+                username: 'user1',
+                password: 'P4ssw0rd',
+                enabled: true).save(failOnError: true)
+
+        def adminUser = User.findByUsername('admin') ?: new User(
+                username: 'admin',
+                password: 'P4ssw0rd',
+                enabled: true).save(failOnError: true)
+
+        if (!user1.authorities.contains(userRole)) {
+            UserRole.create user1, userRole
+        }
+
+        if (!adminUser.authorities.contains(adminRole)) {
+            UserRole.create adminUser, adminRole
+        }
+
     }
     def destroy = {
     }
--- grails-app/controllers/com/careworkstech/carworks/CarController.groovy
+++ grails-app/controllers/com/careworkstech/carworks/CarController.groovy
@@ -1,5 +1,8 @@
 package com.careworkstech.carworks
 
+import grails.plugin.springsecurity.annotation.Secured
+
+@Secured(['ROLE_USER'])
 class CarController {
     static scaffold = true
 
