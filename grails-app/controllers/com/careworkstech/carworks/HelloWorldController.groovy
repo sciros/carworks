b62d7ebac312005669767901ea585de95d2d54d2
Dmitry Sharkov <dsharkov@gmail.com>
2014-08-12 01:45:15 -0400

    snapshot-1 - started adding domain

--- grails-app/controllers/com/careworkstech/carworks/HelloWorldController.groovy
+++
@@ -1,8 +0,0 @@
-package com.careworkstech.carworks
-
-class HelloWorldController {
-
-    def index() {
-      [message: 'hello world']
-    }
-}
---
+++ grails-app/domain/com/careworkstech/carworks/Make.groovy
@@ -0,0 +1,8 @@
+package com.careworkstech.carworks
+
+class Make {
+    String name //can be 'def name' but for ORM best to use static typing
+
+    static constraints = {
+    }
+}
